beba48843dabe1073cd32f8c8fb0c30536205600
MPauline26 <68587854+MPauline26@users.noreply.github.com>
2024-03-23 22:00:10 +0100

    Simplification of sh-file

--- Code/mva.sh
+++ Code/mva.sh
@@ -1,104 +1,15 @@
-python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist2 --NTrees 100 --maxdepth 3 --ncuts 50 
+python train_MVA_Top_vs_TTG.py --mva mlp --variables varlist1 --NLayers 7 --LearningRate 0.03 --Sampling 0.3 --SamplingEpoch 0.8
+
+python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 100 --maxdepth 3 --ncuts 50
 
 
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1  --NTrees 25 --maxdepth 1 --ncuts 50 --overwrite 
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 25 --maxdepth 1 --ncuts 50
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 25 --maxdepth 1 --ncuts 100
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 25 --maxdepth 1 --ncuts 200
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 25 --maxdepth 1 --ncuts 400
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 50 --maxdepth 1 --ncuts 50
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 50 --maxdepth 1 --ncuts 100
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 50 --maxdepth 1 --ncuts 200
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 50 --maxdepth 1 --ncuts 400
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 100 --maxdepth 1 --ncuts 50
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 100 --maxdepth 1 --ncuts 100
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 100 --maxdepth 1 --ncuts 200
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 100 --maxdepth 1 --ncuts 400
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 200 --maxdepth 1 --ncuts 50
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 200 --maxdepth 1 --ncuts 100
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 200 --maxdepth 1 --ncuts 200
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 200 --maxdepth 1 --ncuts 400
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 400 --maxdepth 1 --ncuts 50
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 400 --maxdepth 1 --ncuts 100
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 400 --maxdepth 1 --ncuts 200
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 400 --maxdepth 1 --ncuts 400
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 25 --maxdepth 2 --ncuts 50
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 25 --maxdepth 2 --ncuts 100
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 25 --maxdepth 2 --ncuts 200
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 25 --maxdepth 2 --ncuts 400
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 50 --maxdepth 2 --ncuts 50
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 50 --maxdepth 2 --ncuts 100
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 50 --maxdepth 2 --ncuts 200
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 50 --maxdepth 2 --ncuts 400
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 100 --maxdepth 2 --ncuts 50
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 100 --maxdepth 2 --ncuts 100
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 100 --maxdepth 2 --ncuts 200
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 100 --maxdepth 2 --ncuts 400
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 200 --maxdepth 2 --ncuts 50
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 200 --maxdepth 2 --ncuts 100
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 200 --maxdepth 2 --ncuts 200
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 200 --maxdepth 2 --ncuts 400
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 400 --maxdepth 2 --ncuts 50
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 400 --maxdepth 2 --ncuts 100
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 400 --maxdepth 2 --ncuts 200
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 400 --maxdepth 2 --ncuts 400
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 25 --maxdepth 3 --ncuts 50
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 25 --maxdepth 3 --ncuts 100
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 25 --maxdepth 3 --ncuts 200
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 25 --maxdepth 3 --ncuts 400
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 50 --maxdepth 3 --ncuts 50
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 50 --maxdepth 3 --ncuts 100
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 50 --maxdepth 3 --ncuts 200
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 50 --maxdepth 3 --ncuts 400
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 100 --maxdepth 3 --ncuts 50
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 100 --maxdepth 3 --ncuts 100
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 100 --maxdepth 3 --ncuts 200
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 100 --maxdepth 3 --ncuts 400
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 200 --maxdepth 3 --ncuts 50
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 200 --maxdepth 3 --ncuts 100
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 200 --maxdepth 3 --ncuts 200
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 200 --maxdepth 3 --ncuts 400
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 400 --maxdepth 3 --ncuts 50
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 400 --maxdepth 3 --ncuts 100
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 400 --maxdepth 3 --ncuts 200
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 400 --maxdepth 3 --ncuts 400
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 25 --maxdepth 4 --ncuts 50
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 25 --maxdepth 4 --ncuts 100
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 25 --maxdepth 4 --ncuts 200
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 25 --maxdepth 4 --ncuts 400
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 50 --maxdepth 4 --ncuts 50
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 50 --maxdepth 4 --ncuts 100
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 50 --maxdepth 4 --ncuts 200
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 50 --maxdepth 4 --ncuts 400
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 100 --maxdepth 4 --ncuts 50
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 100 --maxdepth 4 --ncuts 100
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 100 --maxdepth 4 --ncuts 200
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 100 --maxdepth 4 --ncuts 400
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 200 --maxdepth 4 --ncuts 50
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 200 --maxdepth 4 --ncuts 100
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 200 --maxdepth 4 --ncuts 200
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 200 --maxdepth 4 --ncuts 400
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 400 --maxdepth 4 --ncuts 50
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 400 --maxdepth 4 --ncuts 100
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 400 --maxdepth 4 --ncuts 200
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 400 --maxdepth 4 --ncuts 400
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 25 --maxdepth 5 --ncuts 50
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 25 --maxdepth 5 --ncuts 100
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 25 --maxdepth 5 --ncuts 200
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 25 --maxdepth 5 --ncuts 400
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 50 --maxdepth 5 --ncuts 50
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 50 --maxdepth 5 --ncuts 100
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 50 --maxdepth 5 --ncuts 200
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 50 --maxdepth 5 --ncuts 400
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 100 --maxdepth 5 --ncuts 50
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 100 --maxdepth 5 --ncuts 100
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 100 --maxdepth 5 --ncuts 200
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 100 --maxdepth 5 --ncuts 400
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 200 --maxdepth 5 --ncuts 50
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 200 --maxdepth 5 --ncuts 100
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 200 --maxdepth 5 --ncuts 200
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 200 --maxdepth 5 --ncuts 400
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 400 --maxdepth 5 --ncuts 50
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 400 --maxdepth 5 --ncuts 100
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 400 --maxdepth 5 --ncuts 200
-#python train_MVA_Top_vs_TTG.py --mva bdt --variables varlist1 --NTrees 400 --maxdepth 5 --ncuts 400
+
+python train_MVA_Top_vs_TTG.py –plotname MVA_selection1 --variables selection1 --NTrees 100 --maxdepth 3 --ncuts 50 --NLayers 7
+
+python train_MVA_Top_vs_TTG.py –plotname MVA_selection2 --variables selection2 --NTrees 100 --maxdepth 3 --ncuts 50 --NLayers 7
+
+python train_MVA_Top_vs_TTG.py –plotname MVA_selection3 --variables selection3 --NTrees 100 --maxdepth 3 --ncuts 50 --NLayers 7
+
+
+
+python train_MVA_Top_vs_TTG.py –plotname MVA_selection1 --variables selection1 --NTrees 800 --maxdepth 3 --ncuts 20 --NLayers 7
